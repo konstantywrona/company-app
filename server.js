428e4f47386b9f0b980598e28ce0955236efdade
Konstanty Wrona <koswro@gmail.com>
2023-05-05 14:21:03 +0200

    Add mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoClient = require('mongodb').MongoClient;
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
@@ -16,7 +17,7 @@ app.use('/api', productsRoutes);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
-})
+});
 
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
