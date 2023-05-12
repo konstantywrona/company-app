1c63c8f6f6a484ca66b2276f52ceb2d0ead5b0fe
Konstanty Wrona <koswro@gmail.com>
2023-05-12 09:15:46 +0200

    Update models

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  department: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Employee', employeeSchema);
--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
   name: { type: String, required: true },
+  client: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Product', productSchema);
